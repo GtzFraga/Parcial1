e378fcfa8f07714bacbb0a51395e5ff576eb50be
GtzFraga <60673876+GtzFraga@users.noreply.github.com>
2020-02-06 16:42:16 -0600

    version final
    quedo listo el de menor de 3 numeros

---
+++ menor.js
@@ -0,0 +1,23 @@
+//comentario 1 linea 
+/*
+varias lineas
+*/
+/*Generar 3 numeros aleatorios 
+rango de de 0 a 19
+mostrar el menor 
+*/
+//al final mostar los 3 numeros generados 
+//para asegurar que funcione bien
+
+var num1=Math.floor(Math.random()*20)+1;
+var num2=Math.floor(Math.random()*20)+1;
+var num3=Math.floor(Math.random()*20)+1;
+
+if(num1<num2);
+    if(num1<num3);
+        console.log(num1);
+            else (num2<num3);
+                 consoloe.log(num2);
+            else 
+            console.log(num3);
+            console.log(num1 +""num2+""num3);
