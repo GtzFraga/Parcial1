37431d95bd015e5a0c76d8b6048c0860f4d08d9c
GtzFraga <60673876+GtzFraga@users.noreply.github.com>
2020-02-25 00:44:31 -0600

    Add files via upload

---
+++ pi.js
@@ -0,0 +1,20 @@
+var PI=1,factorial=1,cont=2,x=4,masm=1,inf=6;   
+var x=Math.pow (numero,con);
+var factorial=1;
+console.log(pi(numero,x,cont));
+
+        for(i=1;i<=inf;i++) 
+
+function pi(numero,x,cont);
+{
+    pi+pi(x-1,cont*masm);
+    cont +=2;
+    masm*=1;
+
+        for(i=1;i<=factorial;i++)
+{
+    factorial*=i;
+}
+    x=x/factorial;
+        return x;
+}
---
+++ sen(x).js
@@ -0,0 +1,21 @@
+
+var seno=1,factorial=1,cont=2,x=4,masm=1,inf=6;   
+var x=Math.pow (numero,con);
+var factorial=1;
+console.log(seno(numero,x,cont));
+
+        for(i=1;i<=inf;i++) 
+
+function seno(numero,x,cont);
+{
+    seno+seno(x,cont*masm);
+    cont +=2;
+    masm*=1;
+
+        for(i=1;i<=factorial;i++)
+{
+    factorial*=i;
+}
+    x=x/factorial;
+        return x;
+}
